39bcb2d189a5ab6f4b4d075e207bef21fe6987b2
Pranv <pranavbharadwaj142@gmail.com>
2020-06-12 18:39:22 +0530

    ToDo List Completed

--- src/App.js
+++ src/App.js
@@ -1,1 +1,50 @@
 import React, { useState } from 'react';
+import TodoList from './ToDoList'
+
+const App=() =>{
+    const[inputList,setInputList]=useState("");
+    const[items,setItems]=useState([]);
+    const itemEvents=(event)=>{
+        setInputList(event.target.value);
+    }
+    const listOfItems=()=>{
+        setItems((oldItems)=>{
+            return[...oldItems,inputList];
+        });
+        setInputList('');
+    }
+    const deleteItem=(id)=>{
+console.log("delete")
+setItems((oldItems)=>{
+    return oldItems.filter((arrElem,index)=>{
+        return index!==id;
+    })
+})
+    }
+return (
+    <>
+    <div className="main_div">
+    <div className="center_div">
+        <br />
+        <h1>ToDo List </h1>
+        <br/>
+        <input type="text" placeholder="Add a item" value={inputList} onChange={itemEvents}/>
+        <button onClick={listOfItems}>➕</button>
+        <ol>
+            { 
+            items.map((itemval,index ) => {
+              return  <TodoList
+              key={index} 
+              id={index}
+                text={itemval} 
+                onSelect={deleteItem}
+                />;
+                })
+                 }
+        </ol>
+    </div>
+    </div>
+    </>
+)
+};
+export default App;
