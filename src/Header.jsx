b47b5cfaffce1b60b67f47dd68aae6664df859c6
Pranv <pranavbharadwaj142@gmail.com>
2020-06-19 15:07:57 +0530

    added responsive features

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,7 +7,7 @@ return(
    <div className="header">
     <img src={logo} alt="CodingThor" height="100" width="130" />
     <h1 className="my-4 mx-1">NoteIt</h1>
-    <h5 className="my-5">developed by CodingThor</h5>
+    <h6 className="my-5">developed by CodingThor</h6>
    </div>
     </>
 )
